9ea204052e6d2d9b9fa947f2cc49b94e01723579
Hiroki Naganuma <hiroki11x@gmail.com>
2015-05-01 18:50:18 +0900

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,155 @@
+#include "ofApp.h"
+
+/*
+ Build Phases の Run Scriptを以下に書き換える
+ 
+ cp -f ../../../libs/fmodex/lib/osx/libfmodex.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/libfmodex.dylib"; install_name_tool -change ./libfmodex.dylib @executable_path/libfmodex.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
+ 
+ cp -f ../../../addons/ofxLeapMotion2/libs/lib/osx/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/libLeap.dylib"; install_name_tool -change ./libLeap.dylib @executable_path/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
+ 
+ mkdir -p "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Resources/"
+ cp -f "$ICON_FILE" "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Resources/"
+
+*/
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    leap.open();
+    
+    cam.setOrientation(ofPoint(-20, 0, 0));
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    fingersFound.clear();
+    simpleHands = leap.getSimpleHands();
+    if( leap.isFrameNew() && simpleHands.size() ){
+        
+        leap.setMappingX(-230, 230, -ofGetWidth()/2, ofGetWidth()/2);
+        leap.setMappingY(90, 490, -ofGetHeight()/2, ofGetHeight()/2);
+        leap.setMappingZ(-150, 150, -200, 200);
+        
+        fingerType fingerTypes[] = {THUMB, INDEX, MIDDLE, RING, PINKY};
+        
+        for(int i = 0; i < simpleHands.size(); i++){
+            for (int f=0; f<5; f++) {
+                int id = simpleHands[i].fingers[ fingerTypes[f] ].id;
+                ofPoint mcp = simpleHands[i].fingers[ fingerTypes[f] ].mcp; // metacarpal
+                ofPoint pip = simpleHands[i].fingers[ fingerTypes[f] ].pip; // proximal
+                ofPoint dip = simpleHands[i].fingers[ fingerTypes[f] ].dip; // distal
+                ofPoint tip = simpleHands[i].fingers[ fingerTypes[f] ].tip; // fingertip
+                fingersFound.push_back(id);
+            }
+        }
+    }
+    leap.markFrameAsOld();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(90, 90, 90), ofColor(30, 30, 30),  OF_GRADIENT_BAR);
+    
+    ofSetColor(200);
+    ofDrawBitmapString("ofxLeapMotion - Example App\nLeap Connected? " + ofToString(leap.isConnected()), 20, 20);
+    
+    cam.begin();
+    
+    ofPushMatrix();
+    ofRotate(90, 0, 0, 1);
+    ofSetColor(20);
+    ofDrawGridPlane(800, 20, false);
+    ofPopMatrix();
+    
+    
+    fingerType fingerTypes[] = {THUMB, INDEX, MIDDLE, RING, PINKY};
+    
+    for(int i = 0; i < simpleHands.size(); i++){
+        bool isLeft        = simpleHands[i].isLeft;
+        ofPoint handPos    = simpleHands[i].handPos;
+        ofPoint handNormal = simpleHands[i].handNormal;
+        
+        ofSetColor(0, 0, 255);
+        ofDrawSphere(handPos.x, handPos.y, handPos.z, 20);
+        ofSetColor(255, 255, 0);
+        ofDrawArrow(handPos, handPos + 100*handNormal);
+        
+        for (int f=0; f<5; f++) {
+            ofPoint mcp = simpleHands[i].fingers[ fingerTypes[f] ].mcp;  // metacarpal
+            ofPoint pip = simpleHands[i].fingers[ fingerTypes[f] ].pip;  // proximal
+            ofPoint dip = simpleHands[i].fingers[ fingerTypes[f] ].dip;  // distal
+            ofPoint tip = simpleHands[i].fingers[ fingerTypes[f] ].tip;  // fingertip
+            
+            ofSetColor(0, 255, 0);
+            ofDrawSphere(mcp.x, mcp.y, mcp.z, 12);
+            ofDrawSphere(pip.x, pip.y, pip.z, 12);
+            ofDrawSphere(dip.x, dip.y, dip.z, 12);
+            ofDrawSphere(tip.x, tip.y, tip.z, 12);
+            
+            ofSetColor(255, 0, 0);
+            ofSetLineWidth(20);
+            ofLine(mcp.x, mcp.y, mcp.z, pip.x, pip.y, pip.z);
+            ofLine(pip.x, pip.y, pip.z, dip.x, dip.y, dip.z);
+            ofLine(dip.x, dip.y, dip.z, tip.x, tip.y, tip.z);
+        }
+    }
+    cam.end();
+}
+
+
+
+
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    // let's close down Leap and kill the controller
+    leap.close();
+}
